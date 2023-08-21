67508e413d0cb9c7c3c761f9a8276454f83da15b
Ashley Gould <agould@ucop.edu>
2023-08-21 15:56:47 -0700

    Initial commit

---
+++ test/uc3-ops-opensearch.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Uc3OpsOpensearch from '../lib/uc3-ops-opensearch-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/uc3-ops-opensearch-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Uc3OpsOpensearch.Uc3OpsOpensearchStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
